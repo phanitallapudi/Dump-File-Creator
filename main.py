374085780e5b9809bcf62b721b552c19a06a0116
Panindhra <63370060+phanitallapudi@users.noreply.github.com>
2023-09-05 22:41:12 +0530

    optimizing the code

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def create_dump_file(file_path, size_in_bytes):
+def create_dump_file(file_path, size_in_bytes = 1024 * 1024 * 1024):
     with open(file_path, 'wb') as f:
         f.write(b'\0' * size_in_bytes)
 
 if __name__ == "__main__":
     desired_size_gb = 1
     file_path = f"dump_file.bin"
-    size_in_bytes = desired_size_gb * 1024 * 1024 * 1024
-    create_dump_file(file_path, size_in_bytes)
+    create_dump_file(file_path)
     print(f"Dump file '{file_path}' created with size {desired_size_gb} GB.")
